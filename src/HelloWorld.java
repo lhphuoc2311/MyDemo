0173186581eead6417035088a4f4d55246ddf71c
Code Guru Mouse <phuoc2311@gmail.com>
2021-10-24 16:25:25 +0700

    intial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+/*
+*  created date: Oct 24, 2021
+*  author: cgm
+*/
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Triệu !!!");
+    }
+    
+}
